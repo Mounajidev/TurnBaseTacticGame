08dd2e202b12e956a8016bcb6ec37ab00a0a6a8c
Mounajidev <lucilo@gmail.com>
2023-04-20 14:09:04 -0300

    update commented summon action that thrwing error

--- Assets/Scripts/Actions/SummonAction.cs
+++ Assets/Scripts/Actions/SummonAction.cs
@@ -1,75 +1,75 @@
-using System.Collections;
-using System;
-using System.Collections.Generic;
-using UnityEngine;
-using Fusion;
+// using System.Collections;
+// using System;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using Fusion;
 
-public class SummonAction : BaseAction
-{
+// public class SummonAction : BaseAction
+// {
 
-    private string summonType;
+//     private string summonType;
 
-    [SerializeField] private NetworkPrefabRef summonObject;
+//     [SerializeField] private NetworkPrefabRef summonObject;
 
 
-    public override void FixedUpdateNetwork()
-    {
-        if (!isActive)
-        {
-            return;
-        }
-        // Create a unique position for the player
-        Vector3 spawnPosition = new Vector3( 2, 0, 0);
-        PlayerRef player = Runner.SetPlayerObject
-        NetworkObject networkPlayerObject2 = Runner.Spawn(summonObject, spawnPosition, Quaternion.identity, player);
+//     public override void FixedUpdateNetwork()
+//     {
+//         if (!isActive)
+//         {
+//             return;
+//         }
+//         // Create a unique position for the player
+//         Vector3 spawnPosition = new Vector3( 2, 0, 0);
+//         PlayerRef player = Runner.SetPlayerObject();
+//         NetworkObject networkPlayerObject2 = Runner.Spawn(summonObject, spawnPosition, Quaternion.identity, player);
 
         
-            ActionComplete();
+//             ActionComplete();
             
 
-    }
+//     }
 
-    //public override void TakeAction(GridPosition gridPosition, Action onActionComplete) 
-    //{
+//     //public override void TakeAction(GridPosition gridPosition, Action onActionComplete) 
+//     //{
 
-    //    totalSpinAmount = 0f;
-    //    ActionStart(onActionComplete);
-    //}
+//     //    totalSpinAmount = 0f;
+//     //    ActionStart(onActionComplete);
+//     //}
 
-    //Networked from aboce : RPC MEthod
-    //[Rpc(sources: RpcSources.InputAuthority, targets: RpcTargets.All)]
-    public override void RPC_TakeAction(GridPosition gridPosition, Action onActionComplete)
-    {
-        RPC_ActionStart(onActionComplete);
-    }
+//     //Networked from aboce : RPC MEthod
+//     //[Rpc(sources: RpcSources.InputAuthority, targets: RpcTargets.All)]
+//     public override void RPC_TakeAction(GridPosition gridPosition, Action onActionComplete)
+//     {
+//         RPC_ActionStart(onActionComplete);
+//     }
 
-    public override string GetActionName()
-    {
-        return "Summon Spirit Wolf";
-    }
+//     public override string GetActionName()
+//     {
+//         return "Summon Spirit Wolf";
+//     }
 
-    public override List<GridPosition> GetValidActionGridPositionList()
-    {
-        List<GridPosition> validGridPositionList = new List<GridPosition>();
-        GridPosition unitGridPosition = unit.GetGridPosition();
+//     public override List<GridPosition> GetValidActionGridPositionList()
+//     {
+//         List<GridPosition> validGridPositionList = new List<GridPosition>();
+//         GridPosition unitGridPosition = unit.GetGridPosition();
 
-        return new List<GridPosition>
-        {
-            unitGridPosition
-        };
-    }
+//         return new List<GridPosition>
+//         {
+//             unitGridPosition
+//         };
+//     }
 
-    public override int GetActionPointsCost()
-    {
-        return 4;
-    }
+//     public override int GetActionPointsCost()
+//     {
+//         return 4;
+//     }
 
-    public override EnemyAIAction GetEnemyAIAction(GridPosition gridPosition)
-    {
-        return new EnemyAIAction
-        {
-            gridPosition = gridPosition,
-            actionValue = 0,
-        };
-    }
-}
+//     public override EnemyAIAction GetEnemyAIAction(GridPosition gridPosition)
+//     {
+//         return new EnemyAIAction
+//         {
+//             gridPosition = gridPosition,
+//             actionValue = 0,
+//         };
+//     }
+// }
